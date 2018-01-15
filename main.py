c77ac261e218bcee4c8f88b1d9285790648b8e43
张帅男 <zhangshuainan@tbkt.cn>
2018-01-15 18:05:50 +0800

    zsn

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 @file: main.py
 @time: 2018/1/15 15:49
 """
-
+import time
 from flask import Flask
 from flask.ext.mail import Mail, Message
 import os
@@ -33,6 +33,36 @@ def index():
     msg.body = "瑶瑶， 你同事长的挺帅的"
     mail.send(msg)
     return "sent"
+
+@app.route('/html/')
+def index_html():
+    content_title = '''<table width="100%%" border="1" cellspacing="0"  >
+      <tr>
+        <td align="center">名字</td>
+        <td align="center">性别</td>
+        <td align="center">年龄</td>
+      </tr>
+    '''
+
+    data_format = '''  <tr>
+    <td align="center">%s</td>
+    <td align="center">%s</td>
+    <td align="center"><font color='red'>%s</font></td>
+    </tr>'''
+
+    data_list = [('小李', '男', '25'), ('小李1', '男', '25'), ('小李2', '男', '25')]
+    content = content_title
+    for obj in data_list:
+        content += data_format % obj
+
+    content += "</table><br/><br/>"
+
+    subject = '孤独者的自白'
+    msg = Message(subject, sender='zhangshuainan@tbkt.cn', recipients=['zhangshuainan@tbkt.cn'])
+    msg.html = content
+    mail.send(msg)
+
+    return "sent_html"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000)
